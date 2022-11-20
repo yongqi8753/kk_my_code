7102038107b892fff2fa9b5607add6b62675c5f2
yongqi8753 <yongqi8753@gmail.com>
2022-11-20 14:17:42 +0800

    ceshi

--- 测试.py
+++ 测试.py
@@ -4,3 +4,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import warnings
+warnings.filterwarnings('ignore')
+# 读取数据
+train = pd.read_csv('train.csv')
